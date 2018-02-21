2d5adfa4b5144e777a9ed1c854b0b5b733e4c71e
Karl H <khansson.nr2@gmail.com>
2018-02-21 11:33:59 +0100

    Changed header into c++ style

--- libtemplate/include/bench_template.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#ifndef BENCH_TEMPLATE_H
-#define BENCH_TEMPLATE_H
-
-void f(); 
-
-#endif //BENCH_TEMPLATE_H
---
+++ libtemplate/include/bench_template.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#ifndef BENCH_TEMPLATE_HPP
+#define BENCH_TEMPLATE_HPP
+
+void f(); 
+
+#endif //BENCH_TEMPLATE_HPP
