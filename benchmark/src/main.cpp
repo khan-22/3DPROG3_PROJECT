9cd4c2c51d75f2f1ed4addf44bf0dcf753fd3bfd
Karl Hansson <khansson.nr2@gmail.com>
2018-02-28 00:26:32 +0100

    Added configuration to startup message...

--- benchmark/src/main.cpp
+++ benchmark/src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 #include <bench_template.hpp>
 
@@ -6,7 +7,14 @@
 #include <bench_vulkan.hpp>
 
 int main() {
-  std::cout << "Hello World" << std::endl;
+#if defined(NDEBUG)
+	std::string build_config = "(RELEASE)";
+#else
+	std::string build_config = "(DEBUG)";
+#endif
+
+  std::cout << "Running Benchmarks... " << build_config << std::endl;
+
 
   { BenchVulkan bv; }
   { BenchOpenGL bo; }
