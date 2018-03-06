62d20d63a25b5393fd7d15f357f5491279f5409b
Karl H <khansson.nr2@gmail.com>
2018-03-06 14:52:42 +0100

    Tested creating a glfw window in the opengl part of the benchmark

--- libbenchopengl/src/bench_opengl.cpp
+++ libbenchopengl/src/bench_opengl.cpp
@@ -2,19 +2,40 @@
 
 #include <iostream>
 
-#include <glbinding/gl/gl.h>
 #include <glbinding/Binding.h>
+#include <glbinding/gl/gl.h>
+
+#include <GLFW/glfw3.h>
 
 BenchOpenGL::BenchOpenGL() : BenchTemplate() {
   //
-  std::cout << "Created BenchOpenGL" << std::endl;
+  GLFWwindow* window = nullptr;
 
-	glbinding::Binding::initialize();
+  if (!glfwInit()) {
+    std::exit(EXIT_FAILURE);
+  }
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
+  window = glfwCreateWindow(800, 600, "Test", nullptr, nullptr);
+  glfwMakeContextCurrent(window);
+  glfwSetWindowShouldClose(window, true);
+
+  glbinding::Binding::initialize();
+
+  // while (!glfwWindowShouldClose(window)) {
+  //   glfwSwapBuffers(window);
+  //   glfwPollEvents();
+  // }
+
+  std::cout << "Created BenchOpenGL" << std::endl;
 }
 
 BenchOpenGL::~BenchOpenGL() {
   //
   std::cout << "Destroyed BenchOpenGL" << std::endl;
+  glfwTerminate();
 }
 
 void BenchOpenGL::initialize() {}
