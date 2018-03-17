6a0b582b4da72813adc684c2b11cbd5dd38c9a38
Capelliexp <the.1man@hotmail.com>
2018-03-17 12:23:20 +0100

    static_casts are op

--- libbenchopengl/src/bench_opengl.cpp
+++ libbenchopengl/src/bench_opengl.cpp
@@ -116,7 +116,8 @@ void BenchOpenGL::singleTriangleDraw(
 
 	for (int i = 0; i < shaderProgramArr.size(); ++i) {
 		gl::glUseProgram(shaderProgramArr.at(i));
-		//gl::glDrawArrays(/**/, 0, 3);
+		//gl::glBindVertexArray();
+		gl::glDrawArrays(static_cast<gl::GLenum>(GL_TRIANGLES), 0, 3);
 	}
 }
 
@@ -126,6 +127,7 @@ void BenchOpenGL::optimalMultipleTriangleDraw(
 	for (int i = 0; i < shaderProgramArr.size(); ++i) {
 		gl::glUseProgram(shaderProgramArr.at(i));
 		for (int j = 0; j < BENCHMARK_M; ++j) {
+			//gl::glBindVertexArray();
 			//gl::glDrawArrays(/**/, 0, 3);
 		}
 	}
@@ -137,6 +139,7 @@ void BenchOpenGL::badMultipleTriangleDraw(
 	for (int i = 0; i < BENCHMARK_M; ++i) {
 		for (int j = 0; j < shaderProgramArr.size(); ++j) {
 			gl::glUseProgram(shaderProgramArr.at(j));
+			//gl::glBindVertexArray();
 			//gl::glDrawArrays(/**/, 0, 3);	//dependant on i
 		}
 	}
