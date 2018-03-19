6920c6a13ad305e5b615b057bf99e99eecb2b15a
Karl Hansson <khansson.nr2@gmail.com>
2018-03-19 21:03:49 +0100

    Made the test a bit faster for the submission

--- libtemplate/include/common.hpp
+++ libtemplate/include/common.hpp
@@ -13,9 +13,9 @@ using namespace std::chrono;
 using Time     = std::chrono::high_resolution_clock::time_point;
 using TimeSpan = std::chrono::duration<double>;
 
-#define BENCHMARK_OUTER_RUNS 10
-#define BENCHMARK_N 2000
-#define BENCHMARK_M 80
+#define BENCHMARK_OUTER_RUNS 5
+#define BENCHMARK_N 400
+#define BENCHMARK_M 400
 
 static_assert(BENCHMARK_N >= BENCHMARK_M,
               "BENCHMARK_N must be GREATER or EQUAL to BENCHMARK_M");
